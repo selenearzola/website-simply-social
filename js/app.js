a6c6082e43e9b7fb9848f3a23065959b55d05a2c
Selene Arzola <selene.arzola@gmail.com>
2017-05-22 22:54:33 -0500

    Project files added.

---
+++ js/app.js
@@ -0,0 +1,41 @@
+$('#btn-add-user').on('click', function(e) {
+  e.preventDefault();
+  validateEmail($('#email').val());
+});
+$('.add-comment').on('click', function(e){
+  e.preventDefault();
+  $('.modal-comment').toggle();
+  $('.modal-overlay').css("display", "block");
+});
+$('.add-user').on('click', function(e){
+  e.preventDefault();
+  $('.modal-user').toggle();
+  $('.modal-overlay').css("display", "block");
+});
+$('.close-button').on('click', function(e) {
+  e.preventDefault();
+  $('.modal-user, .modal-comment').hide();
+  $('.modal-overlay').css("display", "none");
+});
+function validateEmail(email) {
+  $.ajax({
+    type: 'GET',
+    dataType: 'jsonp',
+    url: 'https://bpi.briteverify.com/emails.json',
+    data: {
+      address: email,
+      apikey: '07422695-8c01-4822-a418-9a17fe71f330'
+    }
+  })
+  .done(function( data, textStatus, jqXHR) {
+    if(data.status === 'valid') {
+      $('.response-message').text('Your user has been created.').css('color', '#00B085').show();
+    } else {
+
+      $('.response-message').text('Your email address is not valid, please try another.').css('color', '#FF0000').show();
+
+    }
+  }).fail(function(jqXHR, textStatus, errorThrown ) {
+          // something went wrong
+        });
+}
